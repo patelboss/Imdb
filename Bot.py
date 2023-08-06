754baeb99caf1afec5985dcae12da156c1c2b7ee
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-06 12:42:43 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,62 @@
+import logging
+import requests
+from bs4 import BeautifulSoup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler
+
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Set your bot token here
+BOT_TOKEN = '1829969794:AAE7BRLnznbiLmWcI8qmw_GoudeGzSzZqHo'
+
+# Define IMDb search function using web scraping
+def search_imdb(query):
+    # Implement your web scraping logic here
+    # Make requests to IMDb search URL, parse HTML with Beautiful Soup, and extract relevant data
+    # Replace the following line with your actual scraping code
+    results = [{'title': 'Movie Title', 'summary': 'Movie Summary', 'imdb_url': 'https://www.imdb.com/title/tt1234567/'}]
+    return results
+
+# Define inline query handler
+def inline_query(update, context):
+    query = update.inline_query.query
+    results = []
+
+    if query:
+        imdb_results = search_imdb(query)
+
+        for result in imdb_results:
+            title = result['title']
+            summary = result['summary']
+            imdb_url = result['imdb_url']
+
+            message_content = InputTextMessageContent(f"{title}\n\n{summary}")
+            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("View on IMDb", url=imdb_url)]])
+            results.append({
+                'type': 'article',
+                'id': str(len(results) + 1),
+                'title': title,
+                'input_message_content': message_content,
+                'reply_markup': reply_markup
+            })
+
+    update.inline_query.answer(results)
+
+# Set up the Telegram bot
+def main():
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Set up inline query handler
+    inline_query_handler = InlineQueryHandler(inline_query)
+    dispatcher.add_handler(inline_query_handler)
+
+    # Start the bot
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
