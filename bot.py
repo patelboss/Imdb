c248b79fe707abbcc9d94981eb66e780bbe1f27d
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-06 20:08:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,18 @@
+from health_check import app as health_check_app
 import logging
 from telegram import Update, Chat
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from imdb import IMDb
-from health_check import app as health_check_app
-# Define your bot's token
-TOKEN = '1829969794:AAE7BRLnznbiLmWcI8qmw_GoudeGzSzZqHo'
+import os
+
+# Set up logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# Retrieve the Telegram Bot API Token from the environment variable
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+if not TELEGRAM_BOT_TOKEN:
+    raise ValueError("TELEGRAM_BOT_TOKEN environment variable not set")
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Hello! I'm your IMDb bot. Send me {text} to search on IMDb.")
@@ -14,30 +22,38 @@ def reply_to_content(update: Update, context: CallbackContext) -> None:
         content = update.message.text
         # Extract content between { and }
         content = content[content.find('{')+1:content.find('}')]
+
+        # Search IMDb using 'content' and retrieve results
+        ia = IMDb()
+        search_results = ia.search_movie(content)
         
-    # Search IMDb using 'content' and retrieve results
-    ia = IMDb()
-    search_results = ia.search_movie(content)
-    
-    if search_results:
-        # Get the first search result (you can modify this to show more results)
-        first_result = search_results[0]
-        title = first_result['title']
-        release_date = first_result.get('release date', 'N/A')
-        rating = first_result.get('rating', 'N/A')
-        summary = first_result.get('plot outline', 'No summary available')
+        if search_results:
+            # Get the first search result (you can modify this to show more results)
+            first_result = search_results[0]
+            title = first_result['title']
+            release_date = first_result.get('release date', 'N/A')
+            rating = first_result.get('rating', 'N/A')
+            summary = first_result.get('plot outline', 'No summary available')
 
-        # Compose IMDb search results
-        reply_message = f"IMDb search results for '{content}':\n"
-        reply_message += f"Title: {title}\nRelease Date: {release_date}\nRating: {rating}\nSummary: {summary}"
+            # Compose IMDb search results
+            reply_message = f"IMDb search results for '{content}':\n"
+            reply_message += f"Title: {title}\nRelease Date: {release_date}\nRating: {rating}\nSummary: {summary}"
 
-        # Send IMDb search results to the appropriate chat
-        if update.message.chat.type == Chat.CHANNEL:
-            context.bot.send_message(update.message.chat_id, reply_message)
+            # Send IMDb search results to the appropriate chat
+            if update.message.chat.type == Chat.CHANNEL:
+                context.bot.send_message(update.message.chat_id, reply_message)
+            else:
+                update.message.reply_text(reply_message)
+
+            logging.info(f"'{content}':\n {title}\nRelease Date: {release_date}\nRating: {rating}\nSummary: {summary}")
         else:
+            update.message.reply_text(f"No IMDb results found for '{content}'.")
+            logging.warning(f"No IMDb results found for '{content}'.")
+    else:
         logging.warning("Received an empty or non-text message.")
+
 def main():
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(TELEGRAM_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
@@ -48,4 +64,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
 health_check_app.run(host='0.0.0.0', port=8080)  # Run Flask app for health check
