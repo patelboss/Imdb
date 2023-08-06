ed008e1faf57c57d35499cf85f1be0e17c88d2f7
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-06 16:53:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,73 +1,29 @@
-import logging
-import sys
-import logging.config
-from telegram.ext import Updater, InlineQueryHandler, MessageHandler, Filters
-from telegram import ParseMode
-from utils.imdb_utils import search_imdb
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
-# Load logging configuration from logging.conf file
-logging.config.fileConfig('logging.conf')
+# Define your bot's token
+TOKEN = '1829969794:AAE7BRLnznbiLmWcI8qmw_GoudeGzSzZqHo'
 
-# Create a logger instance for the bot
-logger = logging.getLogger(__name__)
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("Hello! I'm your IMDb bot. Send me {text} to search on IMDb.")
 
-# Set your bot token here
-BOT_TOKEN = '1829969794:AAE7BRLnznbiLmWcI8qmw_GoudeGzSzZqHo'
+def reply_to_content(update: Update, context: CallbackContext) -> None:
+    content = update.message.text
+    # Extract content between { and }
+    content = content[content.find('{')+1:content.find('}')]
+    # Search IMDb using 'content' and retrieve results
+    # Perform IMDb search here
 
-# Define inline query handler
-def inline_query(update, context):
-    query = update.inline_query.query
-    logger.info(f"Received inline query: {query}")
+    # Send IMDb search results to the group
+    update.message.reply_text("IMDb search results: ...")
 
-    results = []
-
-    if query:
-        imdb_results = search_imdb(query)
-
-        for result in imdb_results:
-            title = result['title']
-            summary = result['summary']
-            imdb_url = result['imdb_url']
-
-            message_content = title + "\n\n" + summary + f"\nView on [IMDb]({imdb_url})"
-            results.append({
-                'type': 'article',
-                'id': str(len(results) + 1),
-                'title': title,
-                'input_message_content': message_content,
-                'parse_mode': ParseMode.MARKDOWN,
-            })
-
-    update.inline_query.answer(results)
-
-# Define group message handler
-def group_message(update, context):
-    message = update.message.text
-    logger.info(f"Received group message: {message}")
-
-    # Check if the message is enclosed within curly braces
-    if message.startswith('{') and message.endswith('}'):
-        # Extract the text between curly braces
-        text_inside_braces = message[1:-1]
-        update.message.reply_text(f"Bot received group message: {text_inside_braces}")
-    else:
-        # Ignore messages that are not enclosed within curly braces
-        logger.info("Ignoring group message")
-
-# Set up the Telegram bot
 def main():
-    updater = Updater(token=BOT_TOKEN, use_context=True)
+    updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Set up inline query handler
-    inline_query_handler = InlineQueryHandler(inline_query)
-    dispatcher.add_handler(inline_query_handler)
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_to_content))
 
-    # Set up group message handler
-    group_message_handler = MessageHandler(Filters.chat_type.groups & Filters.text, group_message)
-    dispatcher.add_handler(group_message_handler)
-
-    # Start the bot
     updater.start_polling()
     updater.idle()
 
