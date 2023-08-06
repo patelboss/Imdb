4b7b263031b3058a6181336418ecb8897cf95961
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-06 12:45:52 +0530

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,9 @@
+import logging
+
+# Set up logging
+logging.basicConfig(filename='bot.log',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+# Create a logger instance for the bot
+logger = logging.getLogger(__name__)
