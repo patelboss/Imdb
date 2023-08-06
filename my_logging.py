8d23198ce9025a22a907a081535e68be49325da8
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-06 14:15:01 +0530

    Update and rename my_logging.py to logging.conf

--- my_logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(filename='bot.log',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-# Create a logger instance for the bot
-logger = logging.getLogger(__name__)
