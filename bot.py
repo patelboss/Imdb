0a79bd3e92e9fa6999d4e06c1d6011309b5f5215
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-06 14:14:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,23 @@
-from my_logging import logger
-import requests
-from bs4 import BeautifulSoup
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
+import logging
+import logging.config
 from telegram.ext import Updater, InlineQueryHandler
 from utils.imdb_utils import search_imdb
-import logging
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# Load logging configuration from logging.conf file
+logging.config.fileConfig('logging.conf')
+
+# Create a logger instance for the bot
 logger = logging.getLogger(__name__)
 
 # Set your bot token here
 BOT_TOKEN = '1829969794:AAE7BRLnznbiLmWcI8qmw_GoudeGzSzZqHo'
 
-# Define IMDb search function using web scraping
-def search_imdb(query):
-    # Implement your web scraping logic here
-    # Make requests to IMDb search URL, parse HTML with Beautiful Soup, and extract relevant data
-    # Replace the following line with your actual scraping code
-    results = [{'title': 'Movie Title', 'summary': 'Movie Summary', 'imdb_url': 'https://www.imdb.com/title/tt1234567/'}]
-    return results
-
 # Define inline query handler
 def inline_query(update, context):
     query = update.inline_query.query
     logger.info(f"Received inline query: {query}")
+
+    results = []
 
     if query:
         imdb_results = search_imdb(query)
@@ -34,7 +26,6 @@ def inline_query(update, context):
             title = result['title']
             summary = result['summary']
             imdb_url = result['imdb_url']
-
 
             message_content = InputTextMessageContent(f"{title}\n\n{summary}")
             reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("View on IMDb", url=imdb_url)]])
