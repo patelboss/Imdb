b13c3b2e98707dffaf78f4aca51c3b4dc26360ca
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-07 08:38:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,3 +4,6 @@ from os import environ
 
 # Bot information
 BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = environ.get('SESSION', 'Media_search')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
