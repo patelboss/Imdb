4bf7b1221470f2e57392f61e553fc75908c10094
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-07 17:13:50 +0530

    Delete imdb_utils.py

--- utils/imdb_utils.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def search_imdb(query):
-    # Implement your web scraping logic here
-    # Make requests to IMDb search URL, parse HTML with Beautiful Soup, and extract relevant data
-    # Replace the following line with your actual scraping code
-    results = [{'title': 'Movie Title', 'summary': 'Movie Summary', 'imdb_url': 'https://www.imdb.com/title/tt1234567/'}]
-    return results
