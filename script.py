32d030ed28e561c2829bc6ac899fc2790d7ece1a
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-07 20:27:15 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,83 +1,65 @@
-import logging
-import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import re
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from imdb import IMDb
-from config import API_ID, API_HASH, BOT_TOKEN
+import requests
+from bs4 import BeautifulSoup
 
-# Set up logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Replace with your own bot token
+TOKEN = 'your_bot_token_here'
 
-app = Client(
-    "imdb_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+def start(update, context):
+    update.message.reply_text("Hello! I'm your IMDb bot. Send me a message with text between $ and & to get IMDb top three results.")
 
-# Log when the bot starts
-logging.info("Bot started. Listening for commands and messages...")
-
-def start_command(update, context):
-    update.reply_text("Hello! I'm your IMDb bot. Send me {text} to search on IMDb.")
-
-@Client.on_message(filters.command("start"))
-def start(client, message):
-    start_command(message, None)
-
-@Client.on_message(filters.text & ~filters.command)
-def reply_to_text(client, message):
-    content = message.text
-
-    # Extract text between $ and & using regular expressions
-    match = re.search(r'\$(.*?)\&', content)
-    if match:
-        search_text = match.group(1)
-
-        # Search IMDb using 'search_text' and retrieve results
-        ia = IMDb()
-        search_results = ia.search_movie(search_text)
-
-        if search_results:
-            # Get the first three search results
-            first_three_results = search_results[:3]
-            reply_message = f"IMDb search results for '{search_text}':\n"
-            for result in first_three_results:
-                title = result['title']
-                release_date = result.get('release date', 'N/A')
-                release_year = result.get('year', 'N/A')
-
-                if release_date == 'N/A':
-                    release_info = f"Release Year: {release_year}"
-                else:
-                    release_info = f"Release Date: {release_date}"
-
-                reply_message += f"\nTitle: {title}\n{release_info}"
-
-            # Send IMDb search results to the appropriate chat
-            client.send_message(chat_id=message.chat.id, text=reply_message)
-        else:
-            # No search results found, send a message indicating no queries are related
-            client.send_message(chat_id=message.chat.id, text=f"No Queries Related {search_text}")
-
-@Client.on_message(filters.command("help"))
-def help_command(update, context):
-    update.reply_text("Send me a message containing text between $ and & to search on IMDb.")
-
-def keep_bot_alive():
-    # Send a "ping" message at regular intervals to keep the bot active
-    while True:
-        app.send_message(chat_id="@vdhsmdm", text="Bot is active and running!")
-        app.loop.run_until_complete(asyncio.sleep(3600))  # Sleep for 1 hour
-
-if __name__ == "__main__":
-    # Start the bot and run the event loop
-    app.start()
+def get_imdb_results(update, context):
+    message = update.message.text
+    matches = re.findall(r'\$(.*?)\&', message)
     
-    # Start the keep-alive loop in a separate thread
-    import threading
-    import asyncio
-    alive_thread = threading.Thread(target=keep_bot_alive)
-    alive_thread.start()
+    if not matches:
+        return
+    
+    for query in matches:
+        search_url = f"https://www.imdb.com/find?q={query}&s=tt&ttype=ft&ref_=fn_ft"
+        response = requests.get(search_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        results = soup.find_all("td", class_="result_text")
+        
+        if not results:
+            continue
+        
+        for i, result in enumerate(results[:3]):
+            title = result.a.get_text()
+            href = result.a['href']
+            imdb_url = f"https://www.imdb.com{href}"
+            
+            details_response = requests.get(imdb_url)
+            details_soup = BeautifulSoup(details_response.content, 'html.parser')
+            
+            try:
+                release_date = details_soup.find("a", title="See more release dates").get_text(strip=True)
+            except AttributeError:
+                release_date = details_soup.find("span", class_="nobr").get_text(strip=True)
+            
+            rating = details_soup.find("span", itemprop="ratingValue").get_text(strip=True)
+            
+            reply_text = f"Top {i + 1}:\nTitle: {title}\nRelease Date: {release_date}\nRating: {rating}\nIMDb URL: {imdb_url}"
+            
+            if update.message.chat.type == "group" or update.message.chat.type == "supergroup":
+                context.bot.send_message(update.message.chat.id, reply_text)
+            elif update.message.chat.type == "private":
+                update.message.reply_text(reply_text)
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    
+    start_handler = CommandHandler('start', start)
+    imdb_handler = MessageHandler(Filters.text & ~Filters.command, get_imdb_results)
+    
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(imdb_handler)
+    
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
