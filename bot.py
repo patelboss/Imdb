0630f0a8f1956156693ca3cfd6e780a2885a91cd
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-07 13:25:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 from telegram import BOT_TOKEN
 from telegram.ext import Application
 
-application = Application.builder().token('BOT_TOKEN').build()
-    
-# Register handlers here
+async def run_application():
+    application = Application.builder().token('BOT_TOKEN').build()
 
-# Get the update_queue from which the application fetches the updates to handle
-update_queue = application.update_queue
-start_fetching_updates(update_queue)
+    # Register handlers here
 
-# Start and run the application
-async with application:
-    application.start()
-    
-    # when some shutdown mechanism is triggered:
-    application.stop()
-    
-    
+    # Get the update_queue from which the application fetches the updates to handle
+    update_queue = application.update_queue
+    start_fetching_updates(update_queue)
+
+    # Start and run the application
+    async with application:
+        application.start()
+        # when some shutdown mechanism is triggered:
+        application.stop()
+
+if __name__ == '__main__':
+    run_application()
