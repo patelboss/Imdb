00c5ad8351bc096ffbe0fe07519ab643bb9490e6
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-10 13:44:53 +0530

    Update now.py

--- plugins/now.py
+++ plugins/now.py
@@ -1,98 +1,61 @@
-import os
-from pyrogram import Client, filters, enums
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-import time
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+import os
+import re
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from imdb import IMDb
+from config import API_ID, API_HASH, BOT_TOKEN
 
-@Client.on_message(filters.command(["imdb", 'search']))
-async def imdb_search(client, message):
-    if ' ' in message.text:
-        k = await message.reply('Searching ImDB')
-        r, title = message.text.split(None, 1)
-        movies = await get_poster(title, bulk=True)
-        if not movies:
-            return await message.reply("No results Found")
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{movie.get('title')} - {movie.get('year')}",
-                    callback_data=f"imdb#{movie.movieID}",
-                )
-            ]
-            for movie in movies
-        ]
-        await k.edit('Here is what i found on IMDb', reply_markup=InlineKeyboardMarkup(btn))
-    else:
-        await message.reply('Give me a movie / series Name')
+# Set up logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-@Client.on_callback_query(filters.regex('^imdb'))
-async def imdb_callback(bot: Client, quer_y: CallbackQuery):
-    i, movie = quer_y.data.split('#')
-    imdb = await get_poster(query=movie, id=True)
-    btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"{imdb.get('title')}",
-                    url=imdb['url'],
-                )
-            ]
-        ]
-    message = quer_y.message.reply_to_message or quer_y.message
-    if imdb:
-        caption = IMDB_TEMPLATE.format(
-            query = imdb['title'],
-            title = imdb['title'],
-            votes = imdb['votes'],
-            aka = imdb["aka"],
-            seasons = imdb["seasons"],
-            box_office = imdb['box_office'],
-            localized_title = imdb['localized_title'],
-            kind = imdb['kind'],
-            imdb_id = imdb["imdb_id"],
-            cast = imdb["cast"],
-            runtime = imdb["runtime"],
-            countries = imdb["countries"],
-            certificates = imdb["certificates"],
-            languages = imdb["languages"],
-            director = imdb["director"],
-            writer = imdb["writer"],
-            producer = imdb["producer"],
-            composer = imdb["composer"],
-            cinematographer = imdb["cinematographer"],
-            music_team = imdb["music_team"],
-            distributors = imdb["distributors"],
-            release_date = imdb['release_date'],
-            year = imdb['year'],
-            genres = imdb['genres'],
-            poster = imdb['poster'],
-            plot = imdb['plot'],
-            rating = imdb['rating'],
-            url = imdb['url'],
-            **locals()
-        )
-    else:
-        caption = "No Results"
-    if imdb.get('poster'):
-        try:
-            await quer_y.message.reply_photo(photo=imdb['poster'], caption=caption, reply_markup=InlineKeyboardMarkup(btn))
-        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-            pic = imdb.get('poster')
-            poster = pic.replace('.jpg', "._V1_UX360.jpg")
-            await quer_y.message.reply_photo(photo=poster, caption=caption, reply_markup=InlineKeyboardMarkup(btn))
-        except Exception as e:
-            logger.exception(e)
-            await quer_y.message.reply(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
-        await quer_y.message.delete()
-    else:
-        await quer_y.message.edit(caption, reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
-    await quer_y.answer()
+# Log when the bot starts
+logging.info("Bot started. Listening for commands and messages...")
 
-    
+def start_command(update, context):
+    update.reply_text("Hello! I'm your IMDb bot. Send me {text} to search on IMDb.")
 
-@app.on_message(filters.text & filters.regex(r'^\$.*&$'))
-async def dollar_sign_text(bot, message):
-    text = message.text[1:-1]
-    await message.reply(f"Text between $ and &: {text}")
+@Client.on_message(filters.command("start"))
+def start(client, message):
+    start_command(message, None)
+
+@Client.on_message(filters.text & ~filters.command)
+def reply_to_text(client, message):
+    content = message.text
+
+    # Extract text between $ and & using regular expressions
+    match = re.search(r'\$(.*?)\&', content)
+    if match:
+        search_text = match.group(1)
+
+        # Search IMDb using 'search_text' and retrieve results
+        ia = IMDb()
+        search_results = ia.search_movie(search_text)
+
+        if search_results:
+            # Get the first three search results
+            first_three_results = search_results[:3]
+            reply_message = f"IMDb search results for '{search_text}':\n"
+            for result in first_three_results:
+                title = result['title']
+                release_date = result.get('release date', 'N/A')
+                release_year = result.get('year', 'N/A')
+
+                if release_date == 'N/A':
+                    release_info = f"Release Year: {release_year}"
+                else:
+                    release_info = f"Release Date: {release_date}"
+
+                reply_message += f"\nTitle: {title}\n{release_info}"
+
+            # Send IMDb search results to the appropriate chat
+            client.send_message(chat_id=message.chat.id, text=reply_message)
+        else:
+            # No search results found, send a message indicating no queries are related
+            client.send_message(chat_id=message.chat.id, text=f"No Queries Related {search_text}")
+
+@Client.on_message(filters.command("help"))
+def help_command(update, context):
+    update.reply_text("Send me a message containing text between $ and & to search on IMDb.")
+app.run()
