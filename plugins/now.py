62fb28ed72b77acaeef8451d06ba39f7cf17edd0
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-13 14:56:44 +0530

    Update now.py

--- plugins/now.py
+++ plugins/now.py
@@ -1,60 +1,55 @@
 import logging
 import os
-import re
-from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+import re
 from imdb import IMDb
-from config import API_ID, API_HASH, BOT_TOKEN
+from pymongo import MongoClient
+from config import API_ID, API_HASH, MONGODB_URI, AUTH_CHANNEL
 
-# Set up logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ia = IMDb()
+mongo_client = MongoClient(MONGODB_URI)
+db = mongo_client['my_database']
+collection = db['movie_collection']
 
-# Log when the bot starts
-logging.info("Bot started. Listening for commands and messages...")
-
-def start_command(update, context):
-    update.reply_text("Hello! I'm your IMDb bot. Send me {text} to search on IMDb.")
-
-@Client.on_message(filters.command("start"))
-def start(client, message):
-    start_command(message, None)
-
-@Client.on_message(filters.text)
-def reply_to_text(client, message):
+@Client.on_message(filters.text & filters.chat(AUTH_CHANNEL) & filters.user('user1', 'user2'))
+async def reply_to_text(client, message):
     content = message.text
 
-    # Extract text between $ and & using regular expressions
-    match = re.search(r'\$(.*?)\&', content)
-    if match:
+    # If the message is in a channel and contains text between $ and &
+    if message.chat.type == "channel" and re.search(r'\$(.*?)\&', content):
+        match = re.search(r'\$(.*?)\&', content)
         search_text = match.group(1)
 
         # Search IMDb using 'search_text' and retrieve results
-        ia = IMDb()
         search_results = ia.search_movie(search_text)
 
         if search_results:
-            # Get the first three search results
-            first_three_results = search_results[:3]
-            reply_message = f"IMDb search results for '{search_text}':\n"
-            for result in first_three_results:
-                title = result['title']
-                release_date = result.get('release date', 'N/A')
-                release_year = result.get('year', 'N/A')
+            movie_title = search_results[0]['title']
+
+            # Check if the movie is in the database
+            if collection.find_one({'title': movie_title}):
+                reply_message = f"The movie '{movie_title}' is already in the database."
+            else:
+                # Get IMDb details
+                imdb_movie = search_results[0]
+                title = imdb_movie['title']
+                release_date = imdb_movie.get('release date', 'N/A')
+                release_year = imdb_movie.get('year', 'N/A')
+                rating = imdb_movie.get('rating', 'N/A')
 
                 if release_date == 'N/A':
                     release_info = f"Release Year: {release_year}"
                 else:
                     release_info = f"Release Date: {release_date}"
 
-                reply_message += f"\nTitle: {title}\n{release_info}"
+                rating_stars = '⭐' * int(rating) if rating != 'N/A' else 'N/A'
+                reply_message = f"Title: {title}\n{release_info}\nRating: {rating_stars}"
 
-            # Send IMDb search results to the appropriate chat
-            client.send_message(chat_id=message.chat.id, text=reply_message)
+            # Send the appropriate reply to the channel
+            await client.send_message(message.chat.id, reply_message)
         else:
-            # No search results found, send a message indicating no queries are related
-            client.send_message(chat_id=message.chat.id, text=f"No Queries Related {search_text}")
+            # No search results found, provide a suggestion
+            suggestion_message = "Spelling mistake! Search correct name on Google then request here."
+            await client.send_message(message.chat.id, suggestion_message)
 
-@Client.on_message(filters.command("help"))
-def help_command(update, context):
-    update.reply_text("Send me a message containing text between $ and & to search on IMDb.")
