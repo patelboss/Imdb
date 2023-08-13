6d2ddd7cdfeeabd87df290848c96a79b8ef9f1e2
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-13 18:05:29 +0530

    Update now.py

--- plugins/now.py
+++ plugins/now.py
@@ -20,24 +20,38 @@ async def start_command(client, message):
 @Client.on_message(filters.command("help"))
 async def help_command(client, message):
     await message.reply_text("Send me a message containing text between $ and & to search on IMDb.")
+from pyrogram import Client, filters
+from imdb import IMDb
+from pymongo import MongoClient
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import API_ID, API_HASH, DATABASE_URI, BOT_TOKEN
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.info("Bot started. Listening for commands and messages...")
+
+# Initialize the Pyrogram client
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
 # Function to perform IMDb search and return top 3 results as a string
 def perform_imdb_search(search_text):
     ia = IMDb()
     search_results = ia.search_movie(search_text)
 
     if search_results:
-        top_results = []
+        keyboard = []
         for i, result in enumerate(search_results[:3], start=1):
             title = result['title']
             release_date = result.get('release date', result.get('year', 'N/A'))
-            top_results.append(f"{i}. {title} - {release_date}")
+            keyboard.append([InlineKeyboardButton(f"{i}. {title} - {release_date}", callback_data=title)])
 
-        return "\n".join(top_results)
+        return InlineKeyboardMarkup(keyboard)
     else:
         return None
 
 # Message handler for text messages
-@Client.on_message(filters.text & filters.incoming)
+@app.on_message(filters.text & filters.incoming)
 async def reply_to_text(client, message):
     content = message.text
     chat_id = message.chat.id
@@ -49,22 +63,22 @@ async def reply_to_text(client, message):
         match = re.search(r'\$(.*?)\&', content)
         if match:
             search_text = match.group(1)
-            search_results = perform_imdb_search(search_text)
-            if search_results:
-                await app.send_message(chat_id, f"Top IMDb results:\n{search_results}")
+            inline_keyboard = perform_imdb_search(search_text)
+            if inline_keyboard:
+                await app.send_message(chat_id, "Top IMDb results:", reply_markup=inline_keyboard)
             else:
                 # IMDb search not found, provide a suggestion
                 suggestion_message = "Spelling Galat Hai!"
                 await app.send_message(chat_id, suggestion_message)
         return
     
-    search_results = perform_imdb_search(search_text)
+    inline_keyboard = perform_imdb_search(search_text)
 
-    if search_results:
+    if inline_keyboard:
         if message.chat.type == 'channel':
-            await app.send_message(chat_id, f"Top IMDb results:\n{search_results}")
+            await app.send_message(chat_id, "Top IMDb results:", reply_markup=inline_keyboard)
         else:
-            await message.reply_text("Select a movie:\n" + search_results)
+            await message.reply_text("Select a movie:", reply_markup=inline_keyboard)
     else:
         # IMDb search not found, provide a suggestion
         suggestion_message = "Spelling Galat Hai!"
@@ -74,7 +88,7 @@ async def reply_to_text(client, message):
             await message.reply_text(suggestion_message)
 
 # Callback handler for inline keyboard buttons
-@Client.on_callback_query()
+@app.on_callback_query()
 async def callback_query_handler(client, query):
     title = query.data
     ia = IMDb()
@@ -84,11 +98,10 @@ async def callback_query_handler(client, query):
 
     # Check if the movie is in the database
     if collection.find_one({'title': title}):
-        reply_message = f"The movie '{title}' is already in the database.\nYou Can Search It in @Filmykeedha_search"
+        reply_message = f"The movie '{title}' is already in the database."
     else:
         # Add movie title to the database
         collection.insert_one({'title': title})
-        reply_message = f" Please Add '{title}' to the database.\n👉🏼 Forward it to @iAmRashmiBot"
+        reply_message = f" Please Add '{title}' to the database."
 
     await query.message.edit_text(reply_message)
-
