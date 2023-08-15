8336a2df47336cdff8d80be1e8698c02b82fc20a
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-15 15:21:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,33 @@
-import re, os
+import re
+import os
+import logging
 
-id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+      API_HASH = os.environ.get("API_HASH", "")
 
-API_HASH = os.environ.get("API_HASH", "")
+      API_ID = os.environ.get("API_ID", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+      BOT_SESSION = os.environ.get("BOT_SESSION", "bot")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+      CAPTION = os.environ.get("CAPTION", "")
 
-PORT = os.environ.get("PORT", "8080")
+      FROM_CHANNEL = os.environ.get("FROM_CHANNEL", None)
 
-CAPTION = os.environ.get("CAPTION", "")
+      FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
+
+      OWNER_ID = os.environ.get("OWNER_ID", "")
+
+      LIMIT = os.environ.get("LIMIT", "2500000")
+
+      SKIP_NO = os.environ.get("SKIP_NO", "0")
+
+      SESSION = os.environ.get("SESSION")
+
+      TO_CHANNEL = os.environ.get("TO_CHANNEL", "")
+
+      PORT = os.environ.get("PORT", "8080")
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
