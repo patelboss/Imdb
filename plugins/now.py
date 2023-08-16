6b9a1e9d038d72a69286bbae6e2387be708504e6
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-16 13:53:23 +0530

    Update now.py

--- plugins/now.py
+++ plugins/now.py
@@ -10,18 +10,17 @@ logging.basicConfig(level=logging.INFO,
 # Log when the bot starts
 logging.info("Bot started. Listening for commands and messages...")
 
-
 copied_message_count = 0
 forwarding_active = False
 
 @Client.on_message(filters.command("run") & filters.private)
-def start_forwarding(client, message):
+async def start_forwarding(client, message):
     global forwarding_active
     if forwarding_active:
-        client.send_message(message.chat.id, "Forwarding is already active.")
+        await client.send_message(message.chat.id, "Forwarding is already active.")
     else:
         forwarding_active = True
-        asyncio.ensure_future(forward_messages(client))
+        await forward_messages(client)
 
 @Client.on_message(filters.chat(FROM_CHANNEL) & filters.text)
 async def forward_messages(client, message):
@@ -40,17 +39,17 @@ def show_copied_message_count(client, message):
 
 async def forward_messages(client):
     global forwarding_active
-    async for message in client.iter_history(FROM_CHANNEL):
-        if not forwarding_active:
-            break
+    try:
+        async for message in client.iter_history(FROM_CHANNEL):
+            if not forwarding_active:
+                break
 
-        if message.text:
-            caption = message.caption if message.caption else None
-            await client.send_message(TO_CHANNEL, text=message.text, caption=caption)
+            if message.text:
+                caption = message.caption if message.caption else None
+                await client.send_message(TO_CHANNEL, text=message.text, caption=caption)
 
-        elif message.media:
-            caption = message.caption if message.caption else None
-            await client.send_media(TO_CHANNEL, media=message.media, caption=caption)
-
-    forwarding_active = False
-
+            elif message.media:
+                caption = message.caption if message.caption else None
+                await client.send_media(TO_CHANNEL, media=message.media, caption=caption)
+    finally:
+        forwarding_active = False
