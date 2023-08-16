881e38b90e3f0230ca6bbbda3c1601ac7752633f
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-16 19:51:03 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -30,7 +30,7 @@ async def run(bot, message):
     )
 
     files_count = 0
-    async for message in bot.User.search_messages(from_chat_id=FROM,offset=Config.SKIP_NO,limit=Config.LIMIT,filter=FILTER):
+    async for message in bot.User.search_messages(chat_id=TO,offset=SKIP_NO,limit=LIMIT,filter=FILTER):
         try:
             if message.video:
                 file_name = message.video.file_name
