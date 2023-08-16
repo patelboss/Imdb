023781311e75a8deb1efcdb6ec38432957b8ea51
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-16 16:01:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from pyrogram import Client
 from config import *
 from aiohttp import web
 from plugins.web_support import web_server
-from plugins.user import User
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
