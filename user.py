80024d64fb5e063a24b23838d850b87f379ecdd2
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-16 15:48:38 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Dark Angel
+
+from config import *
+from config import LOGGER
+from pyrogram import Client, __version__
+
+
+class User(Client):
+    def __init__(self):
+        super().__init__(
+               SESSION,
+            api_hash=API_HASH,
+            api_id=API_ID,
+            workers=4
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        return (self, usr_bot_me.id)
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.SESSION")
