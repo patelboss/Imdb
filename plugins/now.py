2e63e5913f8cb6937f3867da28ec9547bf5ddb3d
patelboss <83952959+patelboss@users.noreply.github.com>
2023-08-18 17:00:54 +0530

    Update now.py

--- plugins/now.py
+++ plugins/now.py
@@ -39,8 +39,9 @@ def perform_imdb_search(search_text):
     else:
         return None
         
-# Message handler for regular text messages
-@Client.on_message(filters.incoming & filters.group)
+     
+# Message handler for regular text messages in groups
+@Client.on_message(filters.group & filters.text)
 async def reply_to_text(client, message):
     search_text = message.text
 
@@ -51,35 +52,81 @@ async def reply_to_text(client, message):
         inline_keyboard = perform_imdb_search(search_text)
 
         if inline_keyboard:
-            await message.reply_text("Which one do you want? Choose one:", reply_markup=inline_keyboard)
+            await send_paginated_results(message.chat.id, inline_keyboard)
         else:
             # IMDb search not found, provide a suggestion
             suggestion_message = "No results found for '{}'.".format(search_text)
-            await message.reply_text(suggestion_message)   
-     
+            await message.reply_text(suggestion_message)
+
+
 # Callback handler for inline keyboard buttons
 @Client.on_callback_query()
 async def callback_query_handler(client, query):
     logging.info("Callback query received.")
-    file_ref = query.data  # Get the selected file_ref (file_id)
+    file_id = query.data  # Get the selected file ID (_id)
 
     try:
-        # Retrieve the file details from the database using the file_ref
+        # Retrieve the file details from the database using the file_id
         mongo_client = MongoClient(DATABASE_URI)
         db = mongo_client['TelegramBot']
         collection = db['TelegramBot']
 
-        file_details = collection.find_one({"file_ref": file_ref})
+        file_details = collection.find_one({"_id": file_id})
         if file_details:
             file_name = file_details['file_name']
-            file_ref = file_details['file_ref']
             mime_type = file_details['mime_type']
             file_caption = file_details['caption']
 
-            # Send the file using its file_id (file_ref)
-            await query.message.reply_document(file_ref, caption=file_caption)
+            # Send the file using its file ID (_id)
+            await query.message.reply_document(file_id, caption=file_caption, filename=file_name, mime_type=mime_type)
         else:
-            reply_message = f"No file found for '{file_ref}'."
+            reply_message = f"No file found for '{file_id}'."
             await query.message.edit_text(reply_message)
     except Exception as e:
         logging.error(f"An error occurred: {e}")
+
+# Callback handler for pagination buttons
+@Client.on_callback_query(filters.regex(r'^(prev|next)_\d+$'))
+async def pagination_callback_handler(client, query):
+    callback_data = query.data
+    action, page_num = callback_data.split('_')
+
+    try:
+        page_num = int(page_num)
+        chat_id = query.message.chat.id
+
+        if action == 'prev' and page_num > 0:
+            page_num -= 1
+        elif action == 'next':
+            page_num += 1
+
+        # Retrieve the original inline keyboard and update the pagination buttons
+        inline_keyboard = query.message.reply_markup
+        updated_keyboard = update_pagination_buttons(inline_keyboard, page_num)
+        
+        await query.message.edit_reply_markup(updated_keyboard)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
+# Update pagination buttons in the inline keyboard
+def update_pagination_buttons(inline_keyboard, page_num):
+    results_per_page = 10
+    num_results = len(inline_keyboard.inline_keyboard) - 1  # Subtract 1 for pagination buttons
+    num_pages = (num_results + results_per_page - 1) // results_per_page
+
+    start_idx = page_num * results_per_page
+    end_idx = min(start_idx + results_per_page, num_results)
+    page_keyboard = inline_keyboard.inline_keyboard[start_idx:end_idx]
+
+    # Add pagination buttons (previous and next)
+    pagination_buttons = []
+    if page_num > 0:
+        pagination_buttons.append(InlineKeyboardButton("Previous", callback_data=f"prev_{page_num}"))
+    if page_num < num_pages - 1:
+        pagination_buttons.append(InlineKeyboardButton("Next", callback_data=f"next_{page_num}"))
+    
+    page_keyboard.append(pagination_buttons)
+
+    updated_keyboard = InlineKeyboardMarkup(page_keyboard)
+    return updated_keyboard
+
